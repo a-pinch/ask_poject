6ef28c5f25081b6921023d55358f70338751a5de
bi <bi@bi-VirtualBox.(none)>
2016-05-24 10:37:14 +0300

    create structure script

--- init.sh
+++ init.sh
@@ -1,1 +1,50 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#! /bin/bush
+
+if [[ ! -e ~/web ]]
+then
+	mkdir ~/web
+	echo "~/web created"
+fi
+cd ~/web
+echo "cd ~/web"
+
+if [[ ! -e etc ]]
+then
+	mkdir etc
+	echo "etc created"
+fi
+if [[ ! -e uploads ]]
+then
+	mkdir uploads
+	echo "uploads created"
+fi
+if [[ ! -e public ]]
+then
+	mkdir public
+	echo "public created"
+fi
+
+cd public
+echo "cd public"
+
+if [[ ! -e img ]]
+then
+	mkdir img
+	echo "create img"
+fi
+if [[ ! -e css ]]
+then
+	mkdir css
+	echo "create css"
+fi
+if [[ ! -e js ]]
+then
+	mkdir js
+	echo "create js"
+fi
+
+if [[ ! -h /etc/nginx/sites-enabled/test.conf ]]
+then
+	echo "create link to test.conf"
+	sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+fi
