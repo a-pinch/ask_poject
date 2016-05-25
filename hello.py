c318b4e66c39e6d5eb28d9e813671982f850f9b7
a-pinch <shmat.co@mail.ru>
2016-05-25 16:07:10 +0300

    get query string

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
+from cgi import parse_qs, escape
+
 def application(env, start_response):
-	d = parse_qs(env['QUERY_STRING']
+	d = parse_qs(env['QUERY_STRING'])
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	return [str(d)]
