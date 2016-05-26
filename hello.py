55873f7c3e83094440a3798767f1514653d63668
a-pinch <shmat.co@mail.ru>
2016-05-26 12:20:21 +0000

    hello for box

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 def application(environ, start_response):
     start_response('200 OK', [('content-type', 'text/plain')])
-    return [bytes(environ['QUERY_STRING'].replace('&','\n'), 'utf-8')]
+    return [environ['QUERY_STRING'].replace('&','\n')]
