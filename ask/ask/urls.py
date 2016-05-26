275bc8bc6aa9a9ec4fab9c3361e9e7996a3574c5
a-pinch <shmat.co@mail.ru>
2016-05-26 20:16:25 +0000

    fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$','qa.voews.test'),
-	url(r'^login/$','qa.voews.test'),	
-	url(r'^signup$','qa.voews.test'),
-	url(r'^question/(\d+)$','qa.voews.test'),	
-	url(r'^ask$/.','qa.voews.test'),	
-	url(r'^popular/$','qa.voews.test'),	
-	url(r'^new/$','qa.voews.test'),
+	url(r'^$','qa.views.test'),
+	url(r'^login/$','qa.views.test'),	
+	url(r'^signup$','qa.views.test'),
+	url(r'^question/(\d+)$','qa.views.test'),	
+	url(r'^ask$/.','qa.views.test'),	
+	url(r'^popular/$','qa.views.test'),	
+	url(r'^new/$','qa.views.test'),
 )
