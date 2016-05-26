763fd4074088086c2b5b9b4acf23fc3bff08afe8
a-pinch <shmat.co@mail.ru>
2016-05-26 09:09:32 +0300

    debug

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 def application(env, start_response):
+	resp = env['QUERY_STRING']
+	print('1:\n')
+	print(resp)
+	resp = ''.join(resp)
+	print('2:\n)
+	print(resp)
+	resp = resp.split('&')
+	print('3:\n)
+	print(resp)
+	resp = [item+'\r\n' for item in resp]
 	start_response('200 OK', [('Content-Type', 'text/plain')])
-	return [.join(env['QUERY_STRING'].splitlines(True))]
+	return ['resp']
