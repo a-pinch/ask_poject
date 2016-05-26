339e14395793202c8f5784a2842ab88ebb86351b
a-pinch <shmat.co@mail.ru>
2016-05-26 14:48:15 +0300

    hello complete

--- hello.py
+++ hello.py
@@ -1,13 +1,3 @@
-def application(env, start_response):
-	resp = env['QUERY_STRING']
-	print('1:\n')
-	print(resp)
-	resp = ''.join(resp)
-	print('2:\n)
-	print(resp)
-	resp = resp.split('&')
-	print('3:\n)
-	print(resp)
-	resp = [item+'\r\n' for item in resp]
-	start_response('200 OK', [('Content-Type', 'text/plain')])
-	return ['resp']
+def application(environ, start_response):
+    start_response('200 OK', [('content-type', 'text/plain')])
+    return [bytes(environ['QUERY_STRING'].replace('&','\n'), 'utf-8')]
