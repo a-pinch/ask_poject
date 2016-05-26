0519606769ab5bc12f391257cf83ba61d194a7df
a-pinch <shmat.co@mail.ru>
2016-05-26 13:57:28 +0000

    gunicorn django config

--- init.sh
+++ init.sh
@@ -54,10 +54,6 @@ echo "starting nginx ..."
 sudo /etc/init.d/nginx restart
 
 echo "startin gunicorn ..."
-#cd ~/web
-#sudo gunicorn hello:application --bind 0.0.0.0:8080
-
-sudo ln -s ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-
+cd ~/web
+gunicorn -c /home/box/web/etc/hello:application --daemon
 gunicorn -c /home/box/web/etc/django.py wsgi --daemon
