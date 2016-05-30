8e1e7f3c7dbb06e2315070a760d68a17f8d2a945
a-pinch <shmat.co@mail.ru>
2016-05-30 10:35:07 +0000

    likes in model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,18 +3,25 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class QuestionManager(models.Manager):
+	def new():
+		pass
+	def popular():
+		pass
+
 class Question(models.Model):
+	objects = QuestionManager()
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	rating = models.IntegerField(blank = True)
-	author = models.OneToOneField(User)
-	test = models.CharField(max_length = 10)
-#	likes = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+	author = models.ForeignKey(User)
+	test22 = models.CharField(max_length = 10)
+	likes = models.ManyToManyField(User, related_name='likes_set')
 
 
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	question = models.ForeignKey(Question, on_delete = models.CASCADE)
-	author = models.OneToOneField(User)
+	author = models.ForeignKey(User)
