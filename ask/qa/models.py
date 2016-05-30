abc50885bab11d2014addd4e56963172640f99d5
a-pinch <shamt.co@mail.ru>
2016-05-30 09:43:17 +0000

    test model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,12 +8,13 @@ class Question(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	rating = models.IntegerField(blank = True)
-	author = User()
-	likes = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+	author = models.OneToOneField(User)
+	test = models.CharField(max_length = 10)
+#	likes = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	question = models.ForeignKey(Question, on_delete = models.CASCADE)
-	author = User()
+	author = models.OneToOneField(User)
