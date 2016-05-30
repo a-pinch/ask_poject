59e731ad80325c438f298a8caf422c3bf29b5b97
a-pinch <shmat.co@mail.ru>
2016-05-30 13:42:42 +0000

    test question

--- ask/qa/tests.py
+++ ask/qa/tests.py
@@ -6,12 +6,13 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE']  = "ask.settings"
 
 
-class TestQuestion(TestCase)
+class TestQuestion(TestCase):
     def testQuestion(self):
-    #	question = Question.object.create()
-    assert False, "Test"
-    user,_ = User.objects.get_or_create(username='x', password = 'y')
-    question = Question(title = 'qwe', text = 'qwe', author = user)
-    question.save()
-    assertIsNone(question.id, 'question id is None')
+	from qa.models import Question
+#	question = Question.object.create()
+#       assert False, "Test"
+        user,_ = User.objects.get_or_create(username='x', password = 'y')
+        question = Question(title = 'qwe', text = 'qwe', author = user)
+        question.save()
+        assertIsNone(question.id, 'question id is None')
 
