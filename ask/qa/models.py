eca93ab92806c03704df905be239107f10c98ffa
a-pinch <shmat.co@mail.ru>
2016-05-30 13:52:09 +0000

    final db models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,14 +13,14 @@ class Question(models.Model):
 	objects = QuestionManager()
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
-	rating = models.IntegerField(blank = True)
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User, related_name='likes_set')
 
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question, on_delete = models.CASCADE)
 	author = models.ForeignKey(User)
