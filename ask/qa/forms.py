6da1e24e393504d6ff092d32acc52fe6cfb8d54b
a-pinch <shmat.co@mail.ru>
2016-06-01 08:23:32 +0000

    return data from cleaned methods

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -4,21 +4,21 @@ from qa.models import Question, Answer
 class AskForm(forms.Form):
     title = forms.CharField(max_length = 255)
     text = forms.CharField(widget = forms.Textarea)
-    def __init__(self, *args, **kwargs):
-	super(AskForm, self).__init__(*args, **kwargs)
+#    def __init__(self, *args, **kwargs):
+#	super(AskForm, self).__init__(*args, **kwargs)
     def clean_title(self):
 	title = self.cleaned_data['title']
 	if not title:
-	    raise forms.ValidationError("Title cann't be empty ")
+            raise forms.ValidationError("Title cann't be empty ")
+	return title
     def clean_text(self):
 	text =self.cleaned_data['text']
 	if not text:
 	    raise form.ValidationError("Please enter text")
+	return text
     def save(self):
-#	ask = Question(**self.cleaned_data)
-	ask = Question()
-	ask.save()
-	return ask
+	self.cleaned_data['author_id'] = 1
+	return Question.objects.create(**self.cleaned_data)
 
 class AnswerForm(forms.Form):
     text = forms.CharField(widget = forms.Textarea)
